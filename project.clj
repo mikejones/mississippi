310aa84a2367b199b0a1a9e2a8ede6fec2f85e7a
Michael Jones <michael.daniel.jones@gmail.com>
2011-01-05 11:24:06 +0000

    bumped the version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject mississippi "0.0.1-SNAPSHOT"
-  :description "validations"
+(defproject mississippi "0.0.2-SNAPSHOT"
+  :description "validations for clojure maps"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [compojure "0.5.2"]
@@ -9,10 +9,5 @@
                      [ring/ring-core "0.3.1"]
                      [ring/ring-jetty-adapter "0.3.1"]
                      [ring/ring-devel "0.3.1"]
-                     [lein-run "1.0.0"]
-;;                      [lein-clojars "0.5-SNAPSHOT"]
-                     [swank-clojure "1.2.1"]]
-;;   :run-aliases {:server [mississippi.web start-server]}
-;;   :repositories [["clojars" "http://clojars.org/repo"]]
-  )
+                     [swank-clojure "1.2.1"]])
 
