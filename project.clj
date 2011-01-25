4300ce7251df9d163f014a3374810e9d9a79bb61
Michael Jones <michael.daniel.jones@gmail.com>
2011-01-25 00:26:29 +0000

    removed the web stuff to stop conflicts

--- project.clj
+++ project.clj
@@ -1,13 +1,6 @@
 (defproject mississippi "0.0.2-SNAPSHOT"
   :description "validations for clojure maps"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [compojure "0.5.2"]
-                 [ring/ring-core "0.3.1"]
-                 [ring/ring-jetty-adapter "0.3.1"]]
-  :dev-dependencies [[compojure "0.5.2"]
-                     [ring/ring-core "0.3.1"]
-                     [ring/ring-jetty-adapter "0.3.1"]
-                     [ring/ring-devel "0.3.1"]
-                     [swank-clojure "1.2.1"]])
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]])
 
