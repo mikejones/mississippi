ca1cff15d47721ea4f38588e7e52cdc9c38c7772
Michael Jones <michael.daniel.jones@gmail.com>
2011-01-25 00:31:04 +0000

    bumped the version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mississippi "0.0.2-SNAPSHOT"
+(defproject mississippi "0.0.3-SNAPSHOT"
   :description "validations for clojure maps"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
