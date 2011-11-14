28ded0239066613ab62af7828bbacb2067cdb652
Michael Jones <michael.daniel.jones@gmail.com>
2011-11-14 23:35:38 +0000

    removed dev dependancy

--- project.clj
+++ project.clj
@@ -1,6 +1,4 @@
-(defproject mississippi "0.0.3-SNAPSHOT"
+(defproject mississippi "0.0.5-SNAPSHOT"
   :description "validations for clojure maps"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]])
 
